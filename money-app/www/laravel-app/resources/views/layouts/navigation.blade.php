0a34e73e5bcbcdf0daaacda55338c61b40e36c74
Gaganpreet Kaur <gk7@hood.edu>
2022-03-09 12:09:39 -0500

    Updated website logo.

--- money-app/www/laravel-app/resources/views/layouts/navigation.blade.php
+++ money-app/www/laravel-app/resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                    <img src="{{asset('/images/dollar.png')}}" alt="Dollar Symbol" width="50" height="50">
                     </a>
                 </div>
 
