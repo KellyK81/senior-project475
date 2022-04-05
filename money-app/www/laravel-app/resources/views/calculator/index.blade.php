8c373c20b971b66d65e7611337c8ebcb5e00712b
Nathaniel Rowe <rowgen97@gmail.com>
2022-04-04 21:36:34 -0400

    Add Retirement Calculator HTML Code

--- money-app/www/laravel-app/resources/views/calculator/index.blade.php
+++ money-app/www/laravel-app/resources/views/calculator/index.blade.php
@@ -2,6 +2,33 @@
 @section('body')
     @parent
     <div class="container">
-        <!-- Remove the comment and paste the html contents here -->
-    </div>
+    <html>
+  
+  <body>
+      <h1>Retirement Calculator</h1>
+      <form oninput="sumresult.value = ((parseInt(A.value) 
+                  + parseInt(B.value)) * (parseFloat(C.value)/100)) + (parseInt(A.value) 
+                  + parseInt(B.value))">
+          <label for="Age">Current Age:</label>
+          <input type="number" name="D" value="22" />
+          <br>
+          <label for="Retire">Retirement Age:</label>
+          <input type="number" name="E" value="65" />
+          <br>
+          <label for="Savings">Current Savings:</label>
+          <input type="number" name="A" value="50" />
+          <br>
+          <label for="Deposit">Annual Deposit:</label>
+          <input type="number" name="B" value="5" />
+          <br>
+          <label for="Interest">Interest Rate:</label>
+          <input type="number" name="C" value="4" />%
+          <br>
+        
+          Total Retirement Savings: $<output name="sumresult"></output>
+      </form>
+  </body>
+    
+  </html>
+    </div> 
 @endsection
