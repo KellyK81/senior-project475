8aa6c6d675d815c876d64e4f100618b0f2e9283c
Gaganpreet Kaur <gk7@hood.edu>
2022-05-03 14:20:31 -0400

    Fixed calculator code

--- money-app/www/laravel-app/resources/views/calculator/index.blade.php
+++ money-app/www/laravel-app/resources/views/calculator/index.blade.php
@@ -233,29 +233,27 @@
                                         <h6 class="m-0 font-weight-bold text-primary">Retirement Calculator</h6>
                                     </div>
                                     <div class="card-body">
-                                    <form oninput="sumresult.value = ((parseInt(A.value) 
-                                            + parseInt(B.value)) * (parseFloat(C.value)/100)) + (parseInt(A.value) 
-                                            + parseInt(B.value))">
-                                        <label for="Age">Current Age:</label>
-                                        <input type="number" name="D" value="22" />
-                                        <br>
-                                        <label for="Retire">Retirement Age:</label>
-                                        <input type="number" name="E" value="65" />
-                                        <br>
-                                        <label for="Savings">Current Savings:</label>
-                                        <input type="number" name="A" value="50" />
-                                        <br>
-                                        <label for="Deposit">Annual Deposit:</label>
-                                        <input type="number" name="B" value="5" />
-                                        <br>
-                                        <label for="Interest">Interest Rate:</label>
-                                        <input type="number" name="C" value="4" />%
-                                        <br>
-                                        
-                                        Total Retirement Savings: $<output name="sumresult"></output>
-                                </form>
-                                        
-                                       
+                                    <form oninput="sumresult.value = (parseInt(E.value) - parseInt(D.value)) 
+                                        * (((parseInt(A.value) + parseInt(B.value)) * (parseInt(C.value)/100))
+                                        + parseInt(A.value) + parseInt(B.value))">
+                                            <label for="Age">Current Age:</label>
+                                            <input type="number" name="D" value="22" />
+                                            <br>
+                                            <label for="Retire">Retirement Age:</label>
+                                            <input type="number" name="E" value="65" />
+                                            <br>
+                                            <label for="Savings">Current Savings:</label>
+                                            <input type="number" name="A" value="50" />
+                                            <br>
+                                            <label for="Deposit">Annual Deposit:</label>
+                                            <input type="number" name="B" value="5000" />
+                                            <br>
+                                            <label for="Interest">Interest Rate:</label>
+                                            <input type="number" name="C" value="4" />%
+                                            <br>
+                                            
+                                            Total Retirement Savings: $<output name="sumresult"></output>
+                                        </form>
                                     </div>
                                 </div>
 
